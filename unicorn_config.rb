abff1a1a18a4940062aca5ab85c1d660a359c4c9
supermomonga <hi@supermomonga.com>
2016-09-17 20:10:19 +0900

    45 workers

--- unicorn_config.rb
+++ unicorn_config.rb
@@ -1,4 +1,4 @@
-worker_processes 32
+worker_processes 45
 preload_app true
 listen "/home/isucon/private_isu/webapp/ruby/unicorn.sock", backlog: 1024
 pid "/home/isucon/private_isu/webapp/ruby/unicorn.pid"
