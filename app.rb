e0a83a7de73b1373fe2f3cae056eb5154fddeebf
Purin Kozuke <p@spacepro.be>
2016-09-17 15:54:39 +0900

    SQL太郎

--- app.rb
+++ app.rb
@@ -238,7 +238,14 @@ module Isuconp
     get '/' do
       me = get_session_user()
 
-      results = db.query('SELECT p.id AS id, p.user_id AS user_id, p.body AS body, p.created_at AS created_at, p.ext AS ext, u.del_flg AS del_flg FROM posts p JOIN users u ON p.user_id = u.id  ORDER BY p.created_at DESC')
+      results = db.query(<<SQL
+SELECT p.id AS id, p.user_id AS user_id, p.body AS body, p.created_at AS created_at, p.ext AS ext, u.del_flg AS del_flg
+FROM posts p JOIN users u ON p.user_id = u.id
+WHERE del_flg = 0
+ORDER BY p.created_at DESC
+SQL
+      )
+
       posts = make_posts(results)
 
       erb :index, layout: :layout, locals: { posts: posts, me: me }
